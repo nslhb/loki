a6edbcb3436619ea14a5e854e5f6483cfd6ff2ac
taruneswar-valeti <80747703+taruneswar-valeti@users.noreply.github.com>
2022-03-28 10:22:05 +0530

    updated orgid

--- tools/lambda-promtail/lambda-promtail/promtail.go
+++ tools/lambda-promtail/lambda-promtail/promtail.go
@@ -168,13 +168,12 @@ func send(ctx context.Context, buf []byte) (int, error) {
 	}
 	req.Header.Set("Content-Type", contentType)
 	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("x-scope-orgid", orgid)
 
 	if username != "" && password != "" {
 		req.SetBasicAuth(username, password)
 	}
-
-    req.Header.Set("X-Scope-OrgID", orgid)
-
+        
 	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
 	if err != nil {
 		return -1, err
